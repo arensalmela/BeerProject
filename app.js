7dc10c2330407a9c391823b920b4c8d5acf7100b
Aren S <adsalmela@gmail.com>
2021-06-12 18:52:23 -0400

    Declared variables to extract specific information

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 //Event listener for API Call. Returns Array
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://api.punkapi.com/v2/beers")
+  fetch("https://api.punkapi.com/v2/beers/random")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data);
+      const name = data[0].name;
+      const suggestedFoodPairing = data[0].food_pairing[0];
+      const beerImage = data[0].image_url;
+      console.log(name);
+      console.log(suggestedFoodPairing);
     });
 });
