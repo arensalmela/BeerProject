547097aa7d0880a129e1e3c4268aa8d6f5608d12
Aren S <adsalmela@gmail.com>
2021-06-13 17:09:11 -0400

    click event for button

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ document.addEventListener("DOMContentLoaded", () => {
   const startBtn = document.querySelector(".beerBtn");
   const randomBeer = document.querySelector(".randomBeer");
   const descriptionDisplay = document.querySelector(".description");
-  const beerImageDisplay = document.querySelector(".imageRoute");
+  //const beerImageDisplay = document.querySelector(".imageRoute");
   const suggestedFoodPairingDisplay = document.querySelector(".foodPairing");
-  fetch("https://api.punkapi.com/v2/beers/random")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const beer = data[0];
-      const name = beer.name;
-      const suggestedFoodPairing = beer.food_pairing[0];
-      const beerImage = beer.image_url;
-      const beerDescription = beer.description;
-      const style = beer.tagline;
-      console.log(data);
-      randomBeer.innerHTML = name + " " + style;
-      descriptionDisplay.innerHTML = beerDescription;
-      suggestedFoodPairingDisplay.innerHTML = suggestedFoodPairing;
-      //beerImageDisplay.innerHTML = beerImage;
-    });
+
+  function data() {
+    fetch("https://api.punkapi.com/v2/beers/random")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const beer = data[0];
+        const name = beer.name;
+        const suggestedFoodPairing = beer.food_pairing[0];
+        const beerImage = beer.image_url;
+        const beerDescription = beer.description;
+        const style = beer.tagline;
+        console.log(data);
+        randomBeer.innerHTML = name + " " + style;
+        descriptionDisplay.innerHTML = beerDescription;
+        suggestedFoodPairingDisplay.innerHTML = suggestedFoodPairing;
+        //beerImageDisplay.innerHTML = beerImage;
+      });
+  }
+  startBtn.addEventListener("click", data);
 });
